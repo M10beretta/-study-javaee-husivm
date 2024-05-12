019bf294b87e72132b14945806b84236abd28b59
m10beretta <mberetta.gm@gmail.com>
2024-05-13 00:28:27 +0300

    commit

--- servlets/src/main/java/com/mber/_007_servlets_and_multithreading/Servlet.java
+++ servlets/src/main/java/com/mber/_007_servlets_and_multithreading/Servlet.java
@@ -3,6 +3,7 @@ package com.mber._007_servlets_and_multithreading;
 import lombok.SneakyThrows;
 
 import javax.servlet.ServletException;
+import javax.servlet.SingleThreadModel;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,26 +14,27 @@ import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
 @WebServlet("007")
-public class Servlet extends HttpServlet {
+public class Servlet extends HttpServlet implements SingleThreadModel {
 
     private int i;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var begin = System.currentTimeMillis();
 
-        for (int j = 0; j < 1_000_000_000; j++) {
+        for (int j = 0; j < 100_000_000; j++) {
             i++;
         }
-        System.out.printf("%s %s%n", Thread.currentThread().getName(), i);
+        System.out.printf("%s %s %d%n", Thread.currentThread().getName(), i, System.currentTimeMillis() - begin);
     }
 
     public static class MultithreadingConnectionTest {
+
         public static void main(String[] args) {
-            var millis = System.currentTimeMillis();
+
             var executorService = Executors.newFixedThreadPool(5);
             IntStream.range(0, 5).forEach(i -> executorService.submit(MultithreadingConnectionTest::connect));
             executorService.shutdown();
-            System.out.println("millis: " + (System.currentTimeMillis() - millis));
         }
 
         @SneakyThrows
