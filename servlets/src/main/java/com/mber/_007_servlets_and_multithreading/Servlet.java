3ee7b37c85901635681321d5c06087ec45b8ba63
m10beretta <mberetta.gm@gmail.com>
2024-05-13 00:46:19 +0300

    commit

--- servlets/src/main/java/com/mber/_007_servlets_and_multithreading/Servlet.java
+++ servlets/src/main/java/com/mber/_007_servlets_and_multithreading/Servlet.java
@@ -3,7 +3,6 @@ package com.mber._007_servlets_and_multithreading;
 import lombok.SneakyThrows;
 
 import javax.servlet.ServletException;
-import javax.servlet.SingleThreadModel;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,21 +10,28 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URL;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.IntStream;
 
 @WebServlet("007")
-public class Servlet extends HttpServlet implements SingleThreadModel {
+public class Servlet extends HttpServlet {
 
-    private int i;
+    private static final ReentrantLock LOCK = new ReentrantLock();
+
+    private static int i;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        var begin = System.currentTimeMillis();
-
-        for (int j = 0; j < 100_000_000; j++) {
-            i++;
+        try {
+            LOCK.lock();
+            var begin = System.currentTimeMillis();
+            for (int j = 0; j < 100_000_000; j++) {
+                i++;
+            }
+            System.out.printf("%s %s %d%n", Thread.currentThread().getName(), i, System.currentTimeMillis() - begin);
+        } finally {
+            LOCK.unlock();
         }
-        System.out.printf("%s %s %d%n", Thread.currentThread().getName(), i, System.currentTimeMillis() - begin);
     }
 
     public static class MultithreadingConnectionTest {
